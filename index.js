28380a9d0c1d39e01ce3e7083631886001a351e7
brianrono <brianrono16@gmail.com>
2023-03-11 01:06:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+let delMain = document.getElementById('main');
+delMain.remove();
+// removes the main#main node from the DOM
+
+let newHeader = document.createElement("h1");
+// creates a new h1 node to the DOM
+
+newHeader.id = "victory";
+// the new header variable is assigned to the h1#victory node
+
+newHeader.textContent = "Brian is the champion";
+// neHeader points to the text content of the h1#victory node with my name
